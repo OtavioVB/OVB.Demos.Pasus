4ee85a144616f97c83f9f610286a1175bd9fe73c
OtavioVB <otaviovb.developer@gmail.com>
2023-11-23 08:51:30 -0300

    feat(student): add `data transfer object`

---
+++ src/OVB.Demos.Eschody.Domain/StudentContext/Student.cs
@@ -0,0 +1,47 @@
+﻿namespace OVB.Demos.Eschody.Domain.StudentContext;
+
+public sealed record Student
+{
+    public Student(
+        Guid id, 
+        
+        Guid correlationId, string sourcePlatform, string executionUser, DateTime createdAt, 
+        
+        string firstName, string lastName, string email, string phone, string password,
+        
+        Guid lastCorrelationId, string lastSourcePlatform, string lastExecutionUser, DateTime lastModifiedAt)
+    {
+        Id = id;
+        CorrelationId = correlationId;
+        SourcePlatform = sourcePlatform;
+        ExecutionUser = executionUser;
+        CreatedAt = createdAt;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Phone = phone;
+        Password = password;
+        LastCorrelationId = lastCorrelationId;
+        LastSourcePlatform = lastSourcePlatform;
+        LastExecutionUser = lastExecutionUser;
+        LastModifiedAt = lastModifiedAt;
+    }
+
+    public Guid Id { get; set; }
+
+    public Guid CorrelationId { get; set; }
+    public string SourcePlatform { get; set; }
+    public string ExecutionUser { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Password { get; set; }
+
+    public Guid LastCorrelationId { get; set; }
+    public string LastSourcePlatform { get; set; }
+    public string LastExecutionUser { get; set; }
+    public DateTime LastModifiedAt { get; set; }
+}
---
+++ tst/OVB.Demos.Eschody.UnitTests/Domain/StudentContext/DataTransferObject/StudentDataTransferObjectTests.cs
@@ -0,0 +1,81 @@
+﻿using OVB.Demos.Eschody.Domain.StudentContext;
+
+namespace OVB.Demos.Eschody.UnitTests.Domain.StudentContext.DataTransferObject;
+
+public sealed class StudentDataTransferObjectTests
+{
+    [Theory]
+    [InlineData("Otavio", "Carmanini", "otavio@gmail.com", "11999999999", "123456789")]
+    [InlineData("", "", "", "", "")]
+    [InlineData("dfdas45543543", "dsa4354353**&", "¨$%¨$%¨$DS", "4345uthfdh", "<script>console.log(\"etc\");</script>")]
+    public void Test_RegularWorkflowAtDataTransferObjectCreation(string firstName, string lastName, string email, string phone, string password)
+    {
+        // Arrange
+        var idStudent = Guid.NewGuid();
+        var correlationId = Guid.NewGuid();
+        var executionUser = "UserTest";
+        var sourcePlatform = ".NET/UnitTests.cs";
+        var createdAt = DateTime.UtcNow;
+
+        // Act
+        var student = new Student(
+            id: idStudent,
+            correlationId: correlationId,
+            sourcePlatform: sourcePlatform,
+            executionUser: executionUser,
+            createdAt: createdAt,
+            firstName: firstName,
+            lastName: lastName,
+            email: email, 
+            phone: phone,
+            password: password,
+            lastCorrelationId: correlationId,
+            lastSourcePlatform: sourcePlatform,
+            lastExecutionUser: executionUser,
+            lastModifiedAt: createdAt);
+
+        // Assert
+        Assert.Equal(
+           expected: idStudent,
+           actual: student.Id);
+        Assert.Equal(
+           expected: correlationId,
+           actual: student.CorrelationId);
+        Assert.Equal(
+           expected: executionUser,
+           actual: student.ExecutionUser);
+        Assert.Equal(
+           expected: sourcePlatform,
+           actual: student.SourcePlatform);
+        Assert.Equal(
+           expected: createdAt,
+           actual: student.CreatedAt);
+        Assert.Equal(
+           expected: correlationId,
+           actual: student.LastCorrelationId);
+        Assert.Equal(
+           expected: executionUser,
+           actual: student.LastExecutionUser);
+        Assert.Equal(
+           expected: sourcePlatform,
+           actual: student.LastSourcePlatform);
+        Assert.Equal(
+           expected: createdAt,
+           actual: student.LastModifiedAt);
+        Assert.Equal(
+            expected: firstName, 
+            actual: student.FirstName);
+        Assert.Equal(
+           expected: lastName,
+           actual: student.LastName);
+        Assert.Equal(
+           expected: email,
+           actual: student.Email);
+        Assert.Equal(
+           expected: password,
+           actual: student.Password);
+        Assert.Equal(
+           expected: phone,
+           actual: student.Phone);
+    }
+}
